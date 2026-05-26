d5a40dd6bf84a793d3cf9af80b5984215975c486
Greddode <patrickrich07@gmail.com>
2026-05-26 05:07:15 -0500

    Remove last sentence from credits

--- game/src/ui/text_data.h
+++ game/src/ui/text_data.h
@@ -2,7 +2,7 @@
 #define TEXT_DATA_H
 
 extern const unsigned char s_controls_data[482];
-extern const unsigned char s_credits_data[598];
+extern const unsigned char s_credits_data[553];
 extern const unsigned char s_story_data[1116];
 
 void RenderTextScreen(const unsigned char* data, int len, const char* backHint);
